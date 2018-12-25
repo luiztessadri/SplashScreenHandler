1d871fb7d0a77bbce6fd08eb51731f904c53960c
luiztessadri <luiztessadri@gmail.com>
2018-12-25 14:37:43 -0200

    app working properly

--- app/src/main/java/com/tessadri/yogi/splashscreenhandler/SplashActivity.java
+++ app/src/main/java/com/tessadri/yogi/splashscreenhandler/SplashActivity.java
@@ -1,13 +1,28 @@
 package com.tessadri.yogi.splashscreenhandler;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
 
+
+    private static final int SPLASH_TIMEOUT = 3000; //3 seconds
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splashIntent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(splashIntent);
+
+                finish();
+            }
+        }, SPLASH_TIMEOUT);
     }
 }
